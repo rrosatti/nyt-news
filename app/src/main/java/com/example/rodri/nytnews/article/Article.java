ad4c83ccde3f617c2d804ae3cc571cd8cd62c67c
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-05-08 14:23:22 -0300

    create Article class

---
+++ app/src/main/java/com/example/rodri/nytnews/article/Article.java
@@ -0,0 +1,45 @@
+package com.example.rodri.nytnews.article;
+
+/**
+ * Created by rodri on 5/8/2016.
+ */
+public class Article {
+
+    private String body;
+    private String date;
+    private String title;
+    private String url;
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+}
---
+++ app/src/main/java/com/example/rodri/nytnews/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.rodri.nytnews.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.rodri.nytnews.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

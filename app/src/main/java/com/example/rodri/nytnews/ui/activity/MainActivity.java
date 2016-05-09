e6001e515096705f7c6a4fb8b23d80ee0d082935
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-05-09 10:02:58 -0300

    create custom input dialog for articles' search

--- app/src/main/java/com/example/rodri/nytnews/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/rodri/nytnews/ui/activity/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.rodri.nytnews.ui.activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -11,10 +17,18 @@ import com.example.rodri.nytnews.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView articlesListView;
+    private EditText etQuery;
+
+    private LayoutInflater inflater;
+    private View view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+        initializeCustomComponents();
         this.setTitle(R.string.articles_title);
     }
 
@@ -29,9 +43,45 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.searchArticlesMenuItem) {
-            Toast.makeText(MainActivity.this, "I'm working!!", Toast.LENGTH_LONG).show();
+            showSearchArticleDialog();
         }
 
         return true;
+    }
+
+    public void initialize() {
+        articlesListView = (ListView)findViewById(R.id.articlesListView);
+        inflater = getLayoutInflater();
+    }
+
+    public void initializeCustomComponents() {
+        view = inflater.inflate(R.layout.custom_search_article_dialog, null);
+
+        etQuery = (EditText) view.findViewById(R.id.etQuery);
+    }
+
+    public void showSearchArticleDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle(R.string.query_dialog_message);
+
+        if (view.getParent() == null) {
+            builder.setView(view);
+        } else {
+            view = null;
+            initializeCustomComponents();
+            builder.setView(view);
+        }
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "query: " + etQuery.getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        builder.show();
+
+
     }
 }

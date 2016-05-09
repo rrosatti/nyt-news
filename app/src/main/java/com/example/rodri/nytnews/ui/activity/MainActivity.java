e44ecdd2d17111ac64a074cc803b003088bf6a1f
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-05-09 09:45:04 -0300

    add search button to the menu layout

--- app/src/main/java/com/example/rodri/nytnews/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/rodri/nytnews/ui/activity/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.rodri.nytnews.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.example.rodri.nytnews.R;
 
@@ -11,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setTitle(R.string.articles_title);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        RelativeLayout relativeLayout = (RelativeLayout) menu.findItem(R.id.searchArticlesMenuItem).getActionView();
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.searchArticlesMenuItem) {
+            Toast.makeText(MainActivity.this, "I'm working!!", Toast.LENGTH_LONG).show();
+        }
+
+        return true;
     }
 }
